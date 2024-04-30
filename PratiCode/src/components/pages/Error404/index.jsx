8862d1492c95bb134e5e366767c627f3bde11ed2
Keven Ferraz de Almeida <167765660+KevenFerraz08@users.noreply.github.com>
2024-04-30 17:19:58 +0000

    ..

--- PratiCode/src/components/pages/Error404/index.jsx
+++ PratiCode/src/components/pages/Error404/index.jsx
@@ -1,10 +1,16 @@
-
+import Styles from './Page404.module.css'
 const Error404 = () => {
     return(
         <>
             <Helmet>
                 <title>page Not found || 404</title>
             </Helmet>
+            <section className={Styles.information_404}>
+                 <div className={Styles.information-container_404}>
+                    <h1>404</h1>
+                    <p>Page not found !</p>
+                </div>
+            </section>
         </>
     )
 }
