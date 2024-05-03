389c8da220331a908684354db6b6f093dc76b6ef
Keven Ferraz de Almeida <167765660+KevenFerraz08@users.noreply.github.com>
2024-05-03 23:09:17 +0000

    create link page tags

--- PratiCode/src/components/pages/Tags/index.jsx
+++ PratiCode/src/components/pages/Tags/index.jsx
@@ -1,7 +1,9 @@
 import { Helmet } from "react-helmet"
+import { Link } from 'react-router-dom';
 import Styles from './Tags.module.css'
 import NavBar from "../../NavBar"
 import Footer from "../../Footer"
+import html_Icon from '../../../assets/img/icon/html.svg'
 
 const Tags = () => {
     return(
@@ -11,7 +13,13 @@ const Tags = () => {
             </Helmet>
             <NavBar />
             <div className={Styles.main}>
-
+                <div className="containerLinks">
+                    <div className="pageLink">
+                        <img src={html_Icon} alt="icone do html" />
+                        <h3>Tags HTML</h3>
+                        <Link className={Styles.button}><span>Ver mais</span></Link>
+                    </div>
+                </div>
             </div>
             <Footer />
         </>
