4d3484973f152a0a56c3b86583641c3fc0e7ee64
Keven Ferraz de Almeida <167765660+KevenFerraz08@users.noreply.github.com>
2024-05-06 14:04:57 +0000

    create json

---
+++ PratiCode/src/components/CodExemple/index.jsx
@@ -0,0 +1,18 @@
+import Styles from './cod.module.css'
+const CodExemple = () => {
+    return(
+        <>
+        <div className={Styles.codigoPrint}>
+                <div className={Styles.containerBol}>
+                <div className={Styles.boll1}></div>
+                <div className={Styles.boll2}></div>
+                <div className={Styles.boll3}></div>
+                </div>
+                <div className={Styles.codigo}>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default CodExemple
